beeb16f72b6d07dd542aa55140ee63d398c00209
Shiyu Wang <shiyu3169@gmail.com>
2019-07-18 11:05:13 -0400

    background img

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const rate = document.querySelector('#rate');
 const rateValue = document.querySelector('#rate-value');
 const pitch = document.querySelector('#pitch');
 const pitchValue = document.querySelector('#pitch-value');
+const body = document.querySelector('body');
 
 // Init voices array
 let voices = [];
@@ -37,6 +38,10 @@ if (synth.onvoiceschanged !== undefined) {
 
 // Speak
 const speak = () => {
+  // Add background animation
+  body.style.background = '#141414 url(../img/wave.gif)';
+  body.style.backgroundRepeat = 'repeat-x';
+  body.style.backgroundSize = '100% 100%';
   // Check if speaking
   if (synth.speaking) {
     console.error('Already Speaking...');
@@ -49,6 +54,7 @@ const speak = () => {
     // Speak end
     speakText.onend = e => {
       console.log('Done speaking...');
+      body.style.background = '#141414';
     };
     // Speak error
     speakText.onerror = e => {
